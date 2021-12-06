bad22e4e8869c08149a5e7f391b634b1c170c468
Chloe CARAYON <chloe.carayon@efrei.net>
2021-12-06 22:20:44 +0100

    feat: do preprocessing on dataset #DEP1-4

--- app/__main__.py
+++ app/__main__.py
@@ -1,4 +1,6 @@
 from src.data.make_dataset import generate_raw
+from src.features.build_features import do_preprocessing
 
 if __name__ == "__main__":
-    generate_raw()
+    #generate_raw()
+    do_preprocessing()
