649530b73e9b707eb455e47c9f5a7a2559884c09
spatzcecile <cecile.spatz@efrei.net>
2021-12-12 17:12:27 +0100

    docs: add docstrings #DEP1-16

--- app/src/features/build_features.py
+++ app/src/features/build_features.py
@@ -10,12 +10,26 @@ from src.models.vocabulary import DIC_VOCABULARY
 import contractions
 
 def clean_labels(df):
+    """This function cleans labels in the dataset.
+
+    :param df: dataset to clean
+    :type df: pd.DataFrame
+    :return: cleaned dataset
+    :rtype: pd.DataFrame
+    """
     print(df.sentiment.value_counts())
     cleanup_nums = {"negative": -1, "neutral":0, "positive": 1}
     df['sentiment_score'] = df['sentiment'].replace(cleanup_nums)
     return df
 
 def clean_text(text):
+    """This function cleans text.
+
+    :param text: text to clean
+    :type text: str
+    :return: cleaned text
+    :rtype: str
+    """
     text = str(text).lower()
     text = contractions.fix(text)
     # http links
@@ -34,18 +48,38 @@ def clean_text(text):
 
 
 def build_own_contractions():
+    """This function builds own contractions.
+
+    """
     for k,v in DIC_VOCABULARY.items():
         contractions.add(k, v)
     #dump_pickle(f"{directory_path}models/contractions", contractions)
 
 
 def clean_stopwords(words):
+    """This function cleans words with stopwords.
+
+    :param words: words to clean with stopwords
+    :type words: str
+    :return: sentences
+    :rtype: str
+    """
     STOPWORDS = stopwords.words('english')
     sentences = [[e for e in word if not e in STOPWORDS] for word in words]
     return sentences
 
 
 def preprocessing(text):
+    """This function allows to preprocess text by doing:
+    - build own contractions
+    - tokenize
+    - stopwords
+
+    :param text: text to preprocessed
+    :type text: str
+    :return: sentences
+    :rtype: str
+    """
     build_own_contractions()
 
     # first cleaning
@@ -65,6 +99,13 @@ def preprocessing(text):
 
 
 def join_words_processing(text):
+    """This function allows to joins words.
+
+    :param text: text to joins words
+    :type text: str
+    :return:
+    :rtype: str
+    """
     data = [' '.join(ele) for ele in text]
     if len(data) <1:
         return np.nan
@@ -72,11 +113,21 @@ def join_words_processing(text):
         return ' '.join(data)
 
 def do_preprocessing(dataset: pd.DataFrame, file_name: str):
+    """This function do preprocessing and convert dataset to csv.
+
+    :param dataset: dataset to preprocess
+    :type dataset: pd.DataFrame
+    :param file_name: name of the csv file
+    :type file_name: str
+    """
     dataset['comments_processed'] = dataset.text.apply(preprocessing)
     dataset['comments_join'] = dataset.comments_processed.apply(join_words_processing).astype(str)
     dataset.to_csv(f'{directory_path}data/processed/{file_name}.csv', index=False)
 
 def preprocess_data():
+    """This function allows to preprocess data.
+
+    """
     twitter_dataset = pd.read_csv(f"{directory_path}data/raw/Twitter_Data.csv")
     reddit_dataset = pd.read_csv(f"{directory_path}data/raw/Reddit_Data.csv")
 
--- app/test/test_mlpart.py
+++ app/test/test_mlpart.py
@@ -6,6 +6,9 @@ from src.features.build_features import clean_text, join_words_processing, prepr
 
 class TestML(unittest.TestCase):
     def test_make_dataset(self):
+        """This function test the creation of the dataset from kaggle.
+
+        """
         self.assertEqual(get_raw_kaggle(
             "cosmos98/twitter-and-reddit-sentimental-analysis-dataset",
             f"{directory_path}data/external",
@@ -21,6 +24,9 @@ class TestML(unittest.TestCase):
 
 
     def test_clean_text(self):
+        """This function test the cleaning of the text.
+
+        """
         self.assertEqual(clean_text(
             "This movie is 4now 1 of the worst movie I ever seen. @hans https://stepheniemeyer.com/the-twilight-saga/",
             ), "this movie is 4now 1 of the worst movie i ever seen. ")
@@ -30,6 +36,9 @@ class TestML(unittest.TestCase):
             ), "this movie is **** worst movie i ever seen. ")
 
     def test_clean_stopwords(self):
+        """This function test the cleaning with stopwords.
+
+        """
         self.assertEqual(clean_stopwords(
             [['this', 'movie', 'is', '1', 'of', 'the', 'worst', 'movie', 'i', 'ever', 'watched']],
             ), [['movie', '1', 'worst', 'movie','ever', 'watched']])
@@ -39,6 +48,9 @@ class TestML(unittest.TestCase):
             ), [['perfect', 'movie', 'love']])
 
     def test_preprocessing(self):
+        """This function test preprocessing.
+
+        """
         self.assertEqual(preprocessing(
             "Tbh This movie is the worst movie I ever seen. I won't let it win a oscar omdb",
             ), [['honest', 'movie', 'worst', 'movie', 'ever', 'seen'], ['let','win', 'oscar', 'dead','body']] )
@@ -49,6 +61,9 @@ class TestML(unittest.TestCase):
 
 
     def test_join_words_processing(self):
+        """This function test join words processing.
+
+        """
         self.assertEqual(join_words_processing(
             [['movie', '1', 'worst', 'movie', 'ever', 'seen']],
         ), "movie 1 worst movie ever seen")
